031c838ac1dea623a8ccf377b00fc3b689f593b4
JoeJane <jjose139@my.centennialcollege.ca>
2022-03-19 15:00:36 -0400

    Resolved conflicts

--- server/routes/surveyQuestions.js
+++ server/routes/surveyQuestions.js
@@ -13,16 +13,14 @@ const { findByUsername } = require('../models/surveyQuestions');
     next();
 }*/
 
-<<<<<<< HEAD
 /* GET request for surveys - Get all Survey Questions */
 router.get(
 	'/surveyQuestionList',
 	surveyQuestionsController.displaySurveyQuestionsList
 );
-=======
+
 /* GET request for surveys */
 //router.get('/surveyList', surveyQuestionsController.displaySurveyQuestionsList);
->>>>>>> origin/Release_1.0
 
 /* GET request for add page - CREATE Operation*/
 router.get('/', surveyQuestionsController.displayAddSurveyQuestionsPage);
@@ -30,7 +28,6 @@ router.get('/', surveyQuestionsController.displayAddSurveyQuestionsPage);
 /* POST request for add page - CREATE Operation*/
 router.post('/', surveyQuestionsController.processAddSurveyQuestionsPage);
 
-<<<<<<< HEAD
 /* GET request for edit page - UPDATE Operation*/
 router.get(
 	'/edit/:id',
@@ -41,6 +38,3 @@ router.get(
 router.post('/edit/:id', surveyQuestionsController.processSurveyQuestionUpdate);
 
 module.exports = router;
-=======
-module.exports = router;
->>>>>>> origin/Release_1.0
