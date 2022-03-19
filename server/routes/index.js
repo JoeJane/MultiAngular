ac75a762cea3d6e13bf1fe1a184cd6cd4e12ba5b
JoeJane <jjose139@my.centennialcollege.ca>
2022-03-19 17:26:07 -0400

    manually added Parth's changes

--- server/routes/index.js
+++ server/routes/index.js
@@ -3,6 +3,8 @@ let express = require('express');
 let router = express.Router();
 
 let indexController = require('../controllers/index');
+let surveyController = require('../controllers/survey');
+let surveyQuestionsController = require('../controllers/surveyQuestions');
 
 /* GET Home page. */
 router.get('/', indexController.displayHomePage);
@@ -22,6 +24,21 @@ router.post('/register', indexController.processRegisterPage);
 
 // GET to perform User Logout
 router.get('/logout', indexController.performLogout);
+
+
+router.get('/list', surveyController.displaySurveyList);
+
+/* GET request for add page - CREATE Operation*/
+router.get('/addSurvey', surveyController.displayAddPage);
+
+/* POST request for add page - CREATE Operation*/
+router.post('/addSurvey', surveyController.processAddPage);
+
+/* GET request for add page - CREATE Operation*/
+router.get('/surveyQuestion', surveyQuestionsController.displayAddSurveyQuestionsPage);
+
+/* POST request for add page - CREATE Operation*/
+router.post('/surveyQuestion', surveyQuestionsController.processAddSurveyQuestionsPage);
 
 
 module.exports = router;
