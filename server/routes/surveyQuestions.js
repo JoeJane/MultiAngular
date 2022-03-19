6cad9fb63cc3f6c5b1174a753d743df808e59387
JoeJane <97465784+JoeJane@users.noreply.github.com>
2022-03-19 13:50:14 -0400

    Revert "Developer 301180169 r1.0"

--- server/routes/surveyQuestions.js
+++ server/routes/surveyQuestions.js
@@ -13,16 +13,8 @@ const { findByUsername } = require('../models/surveyQuestions');
     next();
 }*/
 
-<<<<<<< HEAD
-/* GET request for surveys - Get all Survey Questions */
-router.get(
-	'/surveyQuestionList',
-	surveyQuestionsController.displaySurveyQuestionsList
-);
-=======
 /* GET request for surveys */
 //router.get('/surveyList', surveyQuestionsController.displaySurveyQuestionsList);
->>>>>>> origin/Release_1.0
 
 /* GET request for add page - CREATE Operation*/
 router.get('/', surveyQuestionsController.displayAddSurveyQuestionsPage);
@@ -30,17 +22,4 @@ router.get('/', surveyQuestionsController.displayAddSurveyQuestionsPage);
 /* POST request for add page - CREATE Operation*/
 router.post('/', surveyQuestionsController.processAddSurveyQuestionsPage);
 
-<<<<<<< HEAD
-/* GET request for edit page - UPDATE Operation*/
-router.get(
-	'/edit/:id',
-	surveyQuestionsController.displaySurveyQuestionEditPage
-);
-
-/* POST request for edit page - UPDATE Operation*/
-router.post('/edit/:id', surveyQuestionsController.processSurveyQuestionUpdate);
-
 module.exports = router;
-=======
-module.exports = router;
->>>>>>> origin/Release_1.0
