ee11408000b9be12562d04a1d15508806fb7809b
lauripelli <lpellite@my.centennialcollege.ca>
2022-04-01 23:28:40 -0400

    See the Survey Manager option in the nav bar

--- client/src/app/pages/pages.module.ts
+++ client/src/app/pages/pages.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from "@angular/platform-browser";
 import { FormsModule } from "@angular/forms";
 import {HomeComponent} from "./home/home.component";
 import {PartialsModule} from "../partials/partials.module";
+import {SurveyComponent} from "./survey/survey.component";
+import {SurveyQuestionsComponent} from "./surveyQuestions/surveyQuestions.component";
 import {RegisterComponent} from "./register/register.component";
 
 @NgModule({
   imports: [BrowserModule, FormsModule, PartialsModule],
-  declarations: [HomeComponent, RegisterComponent],
-  exports: [HomeComponent, PartialsModule]
+  declarations: [HomeComponent, RegisterComponentt, SurveyComponent, SurveyQuestionsComponent],
+  exports: [HomeComponent, PartialsModule, SurveyComponent, SurveyQuestionsComponent]
 })
 export class PagesModule{}
