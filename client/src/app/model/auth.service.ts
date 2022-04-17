da13cf099927495996823867d7081864d6984423
lauripelli <lpellite@my.centennialcollege.ca>
2022-04-16 20:21:19 -0400

    Remove survey.model in Angular

--- client/src/app/model/auth.service.ts
+++ client/src/app/model/auth.service.ts
@@ -2,16 +2,13 @@ import {Injectable} from '@angular/core';
 import {Observable} from 'rxjs';
 import {RestDataSource} from './rest.datasource';
 import {User} from './user.model';
-import {Survey} from './survey.model';
 
 @Injectable()
 export class AuthService {
   user: User;
-  survey: Survey;
 
   constructor(private datasource: RestDataSource) {
     this.user = new User();
-    this.survey = new Survey();
   }
 
   authenticate(user: User): Observable<any> {
@@ -28,11 +25,6 @@ export class AuthService {
 
   register(user: User): Observable<any> {
     return this.datasource.register(user);
-  }
-
-  addSurvey(survey: Survey) :Observable<any>
-  {
-    return this.datasource.addSurvey(survey);
   }
 
   storeUserData(token: any, user: User): void {
